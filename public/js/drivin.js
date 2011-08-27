7558b9b42c1fabfe41ecfb86609b68fa06f51c82
Leonardo Quixada <lquixada@gmail.com>
2011-08-27 03:00:51 -0300

    Added thumbnails on playlist.

--- public/js/drivin.js
+++ public/js/drivin.js
@@ -27,13 +27,28 @@ var playlist = {
         
         $( 'button#video-add' ).click(function () {
             var input = $( 'input#video-url' ),
-                id, url = input.val(),
+                id,
+                url = input.val(),
                 matches = url.match(/v=(\w+)/);
             
             if ( matches && matches[1] ) {
                 id = matches[1];
+                
                 $.getJSON( 'http://gdata.youtube.com/feeds/api/videos/'+id+'?alt=json', function ( video ) {
-                    $( 'ul#playlist-queue').append( '<li>'+video.entry.title['$t']+'</li>' );
+                    alert(2)
+                    var link = video.entry.link[0].href,
+                        thumb = video.entry.media$group.media$thumbnail[1].url,
+                        title = video.entry.title['$t'];
+
+                    $( 'ul#playlist-queue').append( [
+                        '<li>',
+                            '<a href="'+link+'">',
+                                '<img src="'+thumb+'" width="80" height="60" />',
+                                title,
+                            '</a>',
+                        '</li>'
+                    ].join(''));
+
                     input.val('');
                 } );
             }
