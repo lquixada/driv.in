e2a16488bcda4c06ae6e55ebd7919cf10fa52301
Leonardo Quixada <lquixada@gmail.com>
2011-08-27 01:58:27 -0300

    Playlist adding media directly from youtube while server is not ready to validae it.

--- public/js/drivin.js
+++ public/js/drivin.js
@@ -16,3 +16,25 @@ function onYouTubePlayerReady(playerId) {
     player = document.getElementById("diplayer");
     player.cueVideoById("5NYt1qirBWg");
 }
+
+
+var playlist = {
+    init: function ( options ) {
+        
+        $( 'button#video-add' ).click(function () {
+            var input = $( 'input#video-url' ),
+                id, url = input.val(),
+                matches = url.match(/v=(\w+)/);
+            
+            if ( matches && matches[1] ) {
+                id = matches[1];
+                $.getJSON( 'http://gdata.youtube.com/feeds/api/videos/'+id+'?alt=json', function ( video ) {
+                    $( 'ul#playlist-queue').append( '<li>'+video.entry.title['$t']+'</li>' );
+                    input.val('');
+                } );
+            }
+
+
+        });
+    }
+}
