d62626f16a5ecbeff0502351a47e489448a2e840
Everton J. Carpes <everton.carpes@gmail.com>
2011-08-27 20:44:38 -0300

    Add YouTube integration when video added

--- public/js/drivin-playlist.js
+++ public/js/drivin-playlist.js
@@ -3,12 +3,12 @@ var playlist = {
     init: function ( options ) {
         this.section = $( 'section#playlist' );
         this.ulQueue = this.section.find( 'section#playlist-queue ul' );
-        this.input = this.section.find( 'input#video-url' ); 
+        this.input = this.section.find( 'input#video-url' );
         this.button = this.section.find( 'button#video-add' );
 
         this.bindEvents();
     },
-    
+
     addItem: function ( data ) {
         var video = {
             link: data.entry.link[0].href,
@@ -37,23 +37,9 @@ var playlist = {
 
     bindEvents: function () {
         var that = this;
-
         this.button.click(function () {
           console.log('add video');
-          socket.emit('add video', 'asdasdas');
-
-            // var videoId,
-            //     value = that.input.val(),
-            //     matches = value.match(/v=(\w+)/),
-            //     url;
-            
-            // if ( matches && matches[1] ) {
-            //     videoId = matches[1];
-            //     url = 'http://gdata.youtube.com/feeds/api/videos/'+videoId+'?alt=json';
-                
-            //     $.getJSON( url, $.proxy( that, 'addItem' ) );
-            // }
-
+          socket.emit('add video', that.input.val());
         });
     }
 };
