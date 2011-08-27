9dc5f468afa40196f7f678aba623f782ff208a55
Emerson Macedo <emerleite@gmail.com>
2011-08-27 09:57:06 -0300

    just clean up server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
+require('nko')('YJfSFoK4/CL4kLGY');
+
 var express = require( 'express' ),
     app = express.createServer();
 
-require('nko')('YJfSFoK4/CL4kLGY');
-
-var http = require('http');
-
-app.listen(process.env.NODE_PORT || 8001);
-
-app.use( express.static( __dirname+'/public', { maxAge: 0 } ) );
+app.use(express.static(__dirname + '/public', { maxAge: 0 }));
 
 app.get('/', function(req, res){
     res.send('Go Horse Brazil\n');
 });
+
+app.listen(process.env.NODE_PORT || 8001);
