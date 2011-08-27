aef8dfd1308f32cb79df7f632cfe88af056a73c3
Leonardo Quixada <lquixada@gmail.com>
2011-08-27 00:08:40 -0300

    Server now using express.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+var express = require( 'express' ),
+    app = express.createServer();
+
 require('nko')('YJfSFoK4/CL4kLGY');
 
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Go Horse Brazil\n');
-}).listen(process.env.NODE_PORT || '8001');
+
+app.listen(process.env.NODE_PORT || 8001);
+
+app.use( express.static( __dirname+'/public', { maxAge: 0 } ) );
+
+app.get('/', function(req, res){
+    res.send('Go Horse Brazil\n');
+});
