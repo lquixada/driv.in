42503eb42af641e53232ede610ae835e43fdc537
Emerson Macedo <emerleite@gmail.com>
2011-08-28 11:21:33 -0300

    Now we have a method named join, that ensures youtubeplayer is ready and socket is connected

--- public/js/drivin.js
+++ public/js/drivin.js
@@ -8,10 +8,23 @@ function onYouTubePlayerReady(playerId) {
     player.init( { element: $( '#diplayer' ).get( 0 ) });
     player.bindEvent("onStateChange", "video.stateChanged");
 
+    playerReady = true;
     debugInfo('youtube socket connected:' + socket.connected);
-    if (socket.connected) {
+  
+    // if (socket.connected) {
+    //   socket.emit('join', room);
+    // } else {
+    //   debugInfo('not connected yet');
+    // }
+}
+
+function joinRoom() {
+  setTimeout(function() {
+    if (playerReady && socket.connected) {
+      debugInfo('can join room');
       socket.emit('join', room);
-    } else {
-      debugInfo('not connected yet');
+      return;
     }
+    joinRoom();
+  }, 50);
 }
