64d4a22145edd14134b35b5e7098a8ccce7e666e
Emerson Macedo <emerleite@gmail.com>
2011-08-28 11:06:36 -0300

    Moved video listeners to video script file

--- public/js/drivin-video.js
+++ public/js/drivin-video.js
@@ -8,6 +8,35 @@ var video = {
         socket.on('video ended', function() {
             player.pause();
         });
+
+        socket.on('video added', function(video) {
+            debugInfo(video);
+        });
+
+        socket.on('next video', function(videoId) {
+            debugInfo('received next video');
+            player.loadId( videoId );
+
+            //Play and pause to force buffer
+            player.play();
+            player.pause();
+        });
+
+        socket.on('move forward', function(videoId, seconds) {
+            debugInfo('loading video: ' + videoId);
+            player.loadId( videoId, seconds );
+
+            debugInfo('will seek to ' + seconds + ' seconds');
+            player.seekTo( seconds );
+
+            player.play();
+            player.pause();
+        });
+
+        socket.on('play now', function() {
+            debugInfo('will play now');
+            player.play();
+        });
     },
     stateChanged: function(newState) {
       //nothing
