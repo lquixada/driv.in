4e8d89cd8910baf380537bf21113f155e55773f8
Everton J. Carpes <everton.carpes@gmail.com>
2011-08-30 23:20:30 -0300

    Alter update of playlist head to cleanup when playlist is empty - Fix #12

--- public/js/drivin-playlist.js
+++ public/js/drivin-playlist.js
@@ -12,7 +12,7 @@ var playlist = {
 
         socket.on('next video', function(video) {
             debugInfo('next video ' + video.id);
-            self.killItem(video);
+            self.goNextTrack(video);
 
             self.tomatoes.removeClass('disabled');
         });
@@ -22,6 +22,12 @@ var playlist = {
             self.addItem(video);
         });
 
+
+        socket.on('video ended', function() {
+            if(self.ulQueue.find('li').size() < 1){
+                self.updateCurrentTrack('', '');
+            }
+        });
 
         setInterval(function(){
             var duration = '';
@@ -42,14 +48,19 @@ var playlist = {
         }, 500);
     },
 
-    killItem: function(video){
+
+    updateCurrentTrack: function(title, duration){
+        this.nowPlaying.find('marquee.video-name')
+            .text(title)
+        this.nowPlaying.find('span.video-duration')
+            .text(duration);
+    },
+
+    goNextTrack: function(video){
         this.ulQueue.find('li#' + video.id)
             .fadeOut(300, function() { $(this).remove(); });
 
-        this.nowPlaying.find('marquee.video-name')
-            .text(video.title)
-        this.nowPlaying.find('span.video-duration')
-            .text(video.duration);
+        this.updateCurrentTrack(video.title, video.duration);
     },
 
     addItem: function(video) {
