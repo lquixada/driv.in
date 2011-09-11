18f7583ea8ac4878e38f95088ed3ebe6a6dcfee4
Everton J. Carpes <everton.carpes@gmail.com>
2011-09-11 18:06:26 -0300

    Add Room#removeUser and tests for #addUser and #removeUser - Refs #23

---
+++ test/test-room.js
@@ -0,0 +1,50 @@
+var testCase = require('nodeunit').testCase;
+var Room = require('../lib/room');
+
+module.exports = testCase({
+    setUp: function (callback) {
+        Room.all = {};
+        this.room1 = new Room('room1');
+        this.room2 = new Room('room2');
+
+        this.newUser1 = { id: 123, join: function(){} };
+        this.newUser2 = { id: 456, join: function(){} };
+        this.newUser3 = { id: 789, join: function(){} };
+
+        return callback();
+    },
+
+    tearDown: function (callback) { return callback(); },
+
+    'should allow users to join rooms': function(test) {
+        var self = this;
+        self.room1.addUser(self.newUser1, function(u){
+            test.equal(self.newUser1, u);
+            test.equal(1, self.room1.users.length);
+            test.done();
+        });
+    },
+
+    'should not allow users to join twice': function(test) {
+        var self = this;
+        self.room1.addUser(self.newUser1, function(u1){
+            self.room1.addUser(self.newUser1, function(u2){
+                test.equal(self.newUser1, u1);
+                test.equal(self.newUser1, u2);
+
+                test.equal(1, self.room1.users.length);
+                test.done();
+            });
+        });
+    },
+
+    'should allow users to leave the room': function(test){
+        var self = this;
+
+        self.room1.users = [self.newUser1, self.newUser2];
+        self.room1.removeUser(self.newUser1, function(u){
+            test.equal(1, self.room1.users.length);
+            test.done();
+        });
+    }
+});
