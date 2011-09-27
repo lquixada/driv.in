c95ff2c3f28b5b680d70ccf35ef333a84371a764
Leonardo Quixada <lquixada@gmail.com>
2011-09-26 23:48:24 -0300

    room is now objectified.

--- public/js/drivin.js
+++ public/js/drivin.js
@@ -8,60 +8,67 @@ function onYouTubePlayerReady() {
     logger.log('youtube socket connected:' + socket.connected);
 }
 
-function joinRoom() {
-    var timer = setInterval(function() {
-        if (player.ready && socket.connected) {
-            logger.log('can join room');
-            audience.init();
-            socket.emit('join', room.name, 'user');
+var room = {
+    name: 'room1',
+    join: function () {
+        var that = this;
 
-            chat.init({
-                userName: 'user'
-            });
+        this.timer = setInterval(function() {
+            if (player.ready && socket.connected) {
+                logger.log('can join room');
+                audience.init();
+                socket.emit('join', room.name, 'user');
 
-            socket.on('chat message', function(message) {
-                $.publish('user-message-received', message);
-            });
+                chat.init({
+                    userName: 'user'
+                });
+                
+                /* Publishers */
+                socket.on('chat message', function(message) {
+                    $.publish('user-message-received', message);
+                });
 
-            socket.on('user join', function(data) {
-                data.avatar = '/img/avatar01.png';
-                $.publish('user-added', data);
-            });
+                socket.on('user join', function(data) {
+                    data.avatar = '/img/avatar01.png';
+                    $.publish('user-added', data);
+                });
 
-            socket.on('user leave', function(data) {
-                $.publish('user-removed', data);
-            });
+                socket.on('user leave', function(data) {
+                    $.publish('user-removed', data);
+                });
 
-            socket.on('next video', function(video) {
-                $.publish( 'video-next', video );
-            });
+                socket.on('next video', function(video) {
+                    $.publish( 'video-next', video );
+                });
 
-            socket.on('video added', function(video) {
-                $.publish( 'video-added', video );
-            });
-            
-            socket.on('video ended', function() {
-                $.publish( 'video-ended' );
-            });
+                socket.on('video added', function(video) {
+                    $.publish( 'video-added', video );
+                });
+                
+                socket.on('video ended', function() {
+                    $.publish( 'video-ended' );
+                });
+                
+                /* Subscribers */
+                $.subscribe('user-message-sent', function(event, message) {
+                    socket.emit('chat message', message.userName, message.userMessage);
+                });
 
-            $.subscribe('user-message-sent', function(event, message) {
-                socket.emit('chat message', message.userName, message.userMessage);
-            });
+                $.subscribe('user-name-changed', function(event, message) {
+                    audience.users[socket.socket.sessionid].name = message.userName;
+                });
 
-            $.subscribe('user-name-changed', function(event, message) {
-                audience.users[socket.socket.sessionid].name = message.userName;
-            });
+                $.subscribe('video-sent', function(event, message) {
+                    socket.emit('add video', message);
+                });
 
-            $.subscribe('video-sent', function(event, message) {
-                socket.emit('add video', message);
-            });
+                $.subscribe('tomato-thrown', function(event, message) {
+                    socket.emit('blame');
+                });
 
-            $.subscribe('tomato-thrown', function(event, message) {
-                socket.emit('blame');
-            });
-
-            clearInterval(timer);
-        }
-    }, 100 );
-}
+                clearInterval(that.timer);
+            }
+        }, 100 );
+    }
+};
 
