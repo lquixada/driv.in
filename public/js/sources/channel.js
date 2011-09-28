d90b25fcc4e187f9d54bfff6c1076e814d97b48c
Leonardo Quixada <lquixada@gmail.com>
2011-09-27 22:43:22 -0300

    Migrating socket references to the channel singleton.

--- public/js/sources/channel.js
+++ public/js/sources/channel.js
@@ -1,5 +1,56 @@
 var channel = {
     init: function () {
+        var that = this;
+
         this.socket = io.connect();
+
+        this.socket.on( 'connect', function () {
+            that.connected = true;
+            that._bindPublishers();
+            that._bindSubscribers();
+        } );
+    },
+
+    _bindPublishers: function () {
+        this.socket.on( 'chat message', function ( message ) {
+            $.publish( 'user-message-received', message );
+        });
+
+        this.socket.on('user join', function(data) {
+            data.avatar = '/img/avatar01.png';
+            $.publish('user-added', data);
+        });
+        
+        this.socket.on( 'user leave', function ( data ) {
+            $.publish( 'user-removed', data );
+        });
+
+        this.socket.on('next video', function(video) {
+            $.publish( 'video-next', video );
+        });
+
+        this.socket.on('video added', function(video) {
+            $.publish( 'video-added', video );
+        });
+
+        this.socket.on('video ended', function() {
+            $.publish( 'video-ended' );
+        });
+    },
+
+    _bindSubscribers: function () {
+        var that = this;
+
+        $.subscribe('user-message-sent', function(event, message) {
+            that.socket.emit('chat message', message.userName, message.userMessage);
+        });
+
+        $.subscribe('video-sent', function(event, message) {
+            that.socket.emit('add video', message);
+        });
+
+        $.subscribe('tomato-thrown', function(event, message) {
+            that.socket.emit('blame');
+        });
     }
 };
--- public/js/specs/channel/channel.spec.js
+++ public/js/specs/channel/channel.spec.js
@@ -1,16 +1,108 @@
 
 describe("Channel", function() {
+    beforeEach(function() {
+        this.data = {};
+        this.socket = {
+            on: function ( eventName, callback ) {
+                this[eventName] = callback;
+            },
+            emit: function ( eventName, data ) {
+                this[eventName](data);
+            }
+        };
 
+        io = {
+            connect: jasmine.createSpy().andReturn( this.socket )
+        };
+
+        channel.init();
+    });
+    
     describe("init", function() {
         it("should connect to socket", function() {
-            io = { connect: jasmine.createSpy().andReturn({}) };
-
-            channel.init();
-
             expect( io.connect ).toHaveBeenCalled();
-            expect( channel.socket ).toBeDefined();
+            expect( channel.socket ).toBe( this.socket );
+        });
+
+        it("should listen to connect event", function() {
+            this.socket.emit( 'connect' );
+
+            expect( channel.connected ).toBe( true );
         });
     });
+
+    describe("publish socket events and data to the pubsub", function() {
+        beforeEach(function() {
+            spyOn( $, 'publish' );
+            
+            this.socket.emit( 'connect' );
+        });
+        
+        it("should publish chat message event", function() {
+            this.socket.emit( 'chat message', this.data );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'user-message-received', this.data );
+        });
+
+        it("should publish user leave event", function() {
+            this.socket.emit( 'user leave', this.data );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'user-removed', this.data );
+        });
+
+        it("should publish user join event", function() {
+            this.socket.emit( 'user join', this.data );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'user-added', this.data );
+            expect( this.data.avatar ).toBe( '/img/avatar01.png' );
+        });
+
+        it("should publish next video event", function() {
+            this.socket.emit( 'next video', this.data );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'video-next', this.data );
+        });
+
+        it("should publish video added event", function() {
+            this.socket.emit( 'video added', this.data );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'video-added', this.data );
+        });
+
+        it("should publish video ended event", function() {
+            this.socket.emit( 'video ended' );
+
+            expect( $.publish ).toHaveBeenCalledWith( 'video-ended' );
+        });
+    });
+
+    describe("emit pubsub events and data to the socket", function() {
+        beforeEach(function() {
+            this.socket.emit( 'connect' );
+
+            spyOn( this.socket, 'emit' );
+        });
+
+        it("should emit chat message", function() {
+            this.data = { userName: 'John', userMessage: 'hey' };
+
+            $.publish( 'user-message-sent', this.data );
+            
+            expect( this.socket.emit ).toHaveBeenCalledWith( 'chat message', this.data.userName, this.data.userMessage);
+        });
+
+        it("should emit video sent", function() {
+            $.publish( 'video-sent', this.data );
+            
+            expect( this.socket.emit ).toHaveBeenCalledWith( 'add video', this.data );
+        });
+
+        it("should emit video sent", function() {
+            $.publish( 'tomato-thrown' );
+            
+            expect( this.socket.emit ).toHaveBeenCalledWith( 'blame' );
+        });
+    });    
     
 });
 
